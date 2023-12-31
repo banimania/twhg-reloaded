4d11fd8744782bc8cbcf8dc0d7a4497e70ab5c24
banimania <dani17val+1@gmail.com>
2023-12-31 06:05:39 +0100

    Add Collision Checks

---
+++ utils/needed.hpp
@@ -0,0 +1,8 @@
+#include "../level/level.hpp"
+
+enum GameState { MENU, EDITOR, PLAYING };
+
+namespace TWHGReloaded {
+  GameState state = PLAYING;
+  Level level = Level(100, 100, Player({100, 100}, 30, 150), Background(CHECKERBOARD, backgroundColorPrimary, backgroundColorSecondary, 40.0f));
+}
